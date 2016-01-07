4012acf06414174031d3d6dfb4d9119238ec5dca
Andrea Flack <andrea.leeflack@gmail.com>
2016-01-07 14:59:22 -0800

    madlib

---
+++ madlib.py
@@ -0,0 +1,13 @@
+madlib = "There are many "
+adjective = input("give adjective: ")
+madlib = madlib + adjective + " ways to choose a  "
+noun = input("give noun: ")
+madlib = madlib + noun + " to read. First, you could ask for recommendation from your homies and "
+plural_noun = input("give plural noun: ")
+madlib += plural_noun + " Just don't ask Aunt "
+name = input("give female name: ") 
+madlib += name + " she only reads "
+adjective = input("give adjective: ")
+madlib += adjective + " books with angry Goddess Figures."
+print(madlib)
+
