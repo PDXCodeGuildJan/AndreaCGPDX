88dcc3cea46350672603ba7b5600e00b6f449a9e
Andrea Flack <andrea.leeflack@gmail.com>
2016-01-11 15:36:39 -0800

    practice

---
+++ practice.py
@@ -0,0 +1,51 @@
+
+def middle_characters (mystring):
+	mid= len(mystring)//2
+	start=mid - 1
+	end= mid + 2
+	print (mystring[start : end])
+	#i am def a vari so i can calling in my main function 
+	#myStringLength = len(mystring)
+	#print (myStringLength)
+	
+# for <var> in <sequence> 
+# ex --   for x in myString:
+#             print(x) 
+#only tab under when there is a colon : b4 
+# it is a belonging part of a block
+def forlp(mystring):
+	print ("poop")	
+	for x in mystring:
+		print(x) 
+
+
+
+def main ():
+	#user_input= input("Please insert word: ")
+	#middle_characters (user_input)
+	#loop (user_input)
+
+	mystring = "I love to art"
+	print (mystring)
+	print (mystring[1])
+	print (mystring[-3])
+	print (mystring[5:8])
+
+
+	middle_characters(mystring)
+	forlp("poop")
+	#called a fun 
+
+
+
+
+
+
+
+	
+
+
+
+
+main()
+
