6a7e93fadef03dad9066b177dd3002ae7a62047b
Andrea Flack <andrea.leeflack@gmail.com>
2016-01-15 09:55:25 -0800

    done done done algo sort1

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -2,7 +2,9 @@ def main():
 # find unsorted list 
 	usl=[8,4,6,3,5,2,1]
 
-	selection_sort(usl)
+	print("Unsorted:", usl)
+	sorted_list = selection_sort(usl)
+	print("Sorted:", sorted_list)
 
 def selection_sort(usl):
 	
@@ -14,7 +16,7 @@ def selection_sort(usl):
 	
 	
 	# repeat until there are no more numbers
-	while index < numInlist
+	while index < numInlist:
 		#so the variable should be in the loop it will be used in 
 		#always start looking at the start of the usnorted part of list
 		#that is why we put index there 
@@ -24,7 +26,7 @@ def selection_sort(usl):
 		# find smallest number in list 
 
 		#looping thru unsorted part of list and index indicates begining of unsorted part
-		while nownumIndex < len(usl[index:])
+		while nownumIndex < len(usl):
 		
 			#if the the nmber we are looking at now is smaller then the lowest number in place 0 in the index then...
 			if usl[nownumIndex] < usl[lowIndex]:
@@ -32,16 +34,20 @@ def selection_sort(usl):
 				lowIndex=nownumIndex
 			# now we are looking at the next index 
 			nownumIndex += 1
-	
-		# update the 1st num of the unsorted list to be the next number in the unsorted list    
-		index += 1
-
 
 		# swap smallest number with first number in list 
 		
 		temp= usl[index]
 		usl[index]=usl[lowIndex]
 		usl[lowIndex] =temp
+	
+		# update the 1st num of the unsorted list to be the next number in the unsorted list    
+		index += 1
+
+
+	return usl
+
+
 			
 
 main()
