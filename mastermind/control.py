8dd0e82ed8ecb002c61f8d1d76ec01a0fe308777
Andrea Flack <andrea.leeflack@gmail.com>
2016-02-17 10:20:05 -0800

    control

---
+++ mastermind/control.py
@@ -0,0 +1,16 @@
+class Game(object):
+	"""docstring for Game"""
+	def __init__(self):
+
+		def welcome ():
+			pass
+		def start_round():
+			pass
+
+		"""decide to loop back for valid input
+		 string based on validate_guess_format_template(all_guess[ : ], turn): loop back to start_round():"""
+
+		 """ Decide to loop back for valid input based on 
+		 validate_guess_format_template (). Decide if round won or ongoing 
+		 call draw_b_template(all guesses[], turn) """
+		
