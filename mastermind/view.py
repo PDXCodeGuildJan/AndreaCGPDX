43c0070cda8727598a219395ccd0228ee3c7931d
Andrea Flack <andrea.leeflack@gmail.com>
2016-02-17 10:19:26 -0800

    view

---
+++ mastermind/view.py
@@ -0,0 +1,26 @@
+class Display(object):
+	"""docstring for Display"""
+	def __init__(self):
+		
+	input_template = ""
+
+	def show_display():
+		pass
+	def draw_menu():
+		pass
+	def draw_board_template():
+		pass
+	def display_masters_ballz():
+		pass
+	def draw_win():
+		pass 
+	def validate_guess_format_template(a_str): 
+		pass
+	def draw_loser():
+		pass
+	def get_play_or_quit():
+		pass
+	def get_user_guess():
+		pass
+
+		
