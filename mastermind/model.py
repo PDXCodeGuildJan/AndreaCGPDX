c3c841d62fbd0f45e86e5c862f54c7e902d4979a
shawnwaldow <shawnwaldow@hotmail.com>
2016-02-17 11:35:03 -0800

    Now we have all three modules?

---
+++ mastermind/model.py
@@ -0,0 +1,42 @@
+class ModelStuff(object):
+	"""docstring for ModelStuff"""
+	def __init__(self, arg):
+		self.wins_n_losses = 0
+		self.turn = 0
+		self.masters_ballz = []
+		self.welcome_n_instructions = "Welcome? Finish me!"
+		self.all_guesses = []
+
+	def validate_guess(masters_ballz_guess[Ballz]):
+		pass
+
+class Ballz(object):
+	"""docstring for Ballz"""
+	def __init__(self, arg):
+		self.color = balls[rep]
+		self.balls_rep = {}
+
+
+class PegBin(object):
+	"""docstring for PegBin"""
+	def __init__(self, arg):
+		pegs = []
+
+	def eval_color():
+		pass
+
+	def eval_position():
+		pass
+
+	def sort_pegs():
+		pass
+
+class Guess(object):
+	"""docstring for Guess"""
+	def __init__(self, arg):
+		self.this_guess = []
+		self.a_peg_bin = PegBin()
+		self.guess_number = 0
+
+	def validate_guess(self, masters_ballz[balls]):
+		pass
