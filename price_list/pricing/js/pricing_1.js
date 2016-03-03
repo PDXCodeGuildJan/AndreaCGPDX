acfbefa97ced16b451e0825d3eb53575246e1f73
Andrea Flack <andrea.leeflack@gmail.com>
2016-03-03 12:21:37 -0800

    again noonish

--- price_list/pricing/js/pricing_1.js
+++ price_list/pricing/js/pricing_1.js
@@ -229,10 +229,11 @@ function loadDataWithAJAX(){
    //setup the onload function 
    request.onload = function(){
       if (request.status === 200){
-         console.log(request.responseText)
-      }
+         var prodJSON = (request.responseText)
+        products = JSON.parse(prodJSON);
+         displayInventory();
    }
-
+}
 
 request.send(); 
 }
